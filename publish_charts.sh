d13deb4ed34cf24e5d0f75cd03c960e63c243e56
Pythoner6 <pythoner6@gmail.com>
2023-12-15 19:35:21 -0500

    fix workflow error

--- publish_charts.sh
+++ publish_charts.sh
@@ -1,2 +1,3 @@
-echo "$TOKEN" | helm registry login "$REGISTRY" -u "$ACTOR" --pasword-stdin
+set -eo pipefail
+echo "$TOKEN" | helm registry login "$REGISTRY" -u "$ACTOR" --password-stdin
 find result/ -name '*.tgz' -execdir helm push "{}" "oci://$REGISTRY" \;
