085751463d16ca4e7268b4cefd9ddabc2bdeb439
Pythoner6 <pythoner6@gmail.com>
2023-12-20 03:24:18 -0500

    fix: helper script

--- tools/scripts/oci_helpers.sh
+++ tools/scripts/oci_helpers.sh
@@ -1,9 +1,9 @@
 get_chart_name() {
-  jq -r '.manifests[0].annotations["org.openconatiners.image.ref.name"]' "$config"
+  jq -r '.manifests[0].annotations["org.openconatiners.image.ref.name"]' "$1/index.json"
 }
 
 get_chart_version() {
-  jq -r '.manifests[0].annotations["org.openconatiners.image.version"]' "$config"
+  jq -r '.manifests[0].annotations["org.openconatiners.image.version"]' "$1/index.json"
 }
 
 get_chart_digest() {
