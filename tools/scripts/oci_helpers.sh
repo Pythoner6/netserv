4ba8b9cca563397849a778ad5c25f9c8dee00361
Pythoner6 <pythoner6@gmail.com>
2023-12-20 02:42:00 -0500

    fix: helm versions have to be semver

--- tools/scripts/oci_helpers.sh
+++ tools/scripts/oci_helpers.sh
@@ -4,6 +4,15 @@ get_chart_name() {
   jq -r '.name' "$config"
 }
 
+get_chart_version() {
+  digest="$(jq -r '.manifests[0].digest | sub(":"; ".")' "$1/index.json")"
+  manifest="$1/blobs/$(jq -r '.manifests[0].digest | sub(":";"/")' "$1/index.json")"
+  config="$1/blobs/$(jq -r '.config.digest | sub(":";"/")' "$manifest")"
+  version="$(jq -r '.version' "$config")"
+  name="$(jq -r '.name' "$config")"
+  echo "${version#v}+$digest"
+}
+
 get_chart_digest() {
   jq -r '.manifests[0].digest | sub("^[^:]*:"; "")' "$1/index.json"
 }
